f4280dc31135597f8d325fa644f080cd8d209fbd
rudakow <kinorururu007@gmail.com>
2023-11-01 19:58:58 +0500

    Initial commit

--- src/main/java/uz/najottalim/imtihon/Repository/BolimRepository.java
+++ src/main/java/uz/najottalim/imtihon/Repository/BolimRepository.java
@@ -3,5 +3,5 @@ package uz.najottalim.imtihon.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.najottalim.imtihon.Entitys.Bolim;
 
-public interface BolimRepository extends JpaRepository<Long, Bolim> {
+public interface BolimRepository extends JpaRepository<Bolim, Long> {
 }
--- src/main/java/uz/najottalim/imtihon/Repository/MalumotRypository.java
+++ src/main/java/uz/najottalim/imtihon/Repository/MalumotRypository.java
@@ -1,8 +1,11 @@
 package uz.najottalim.imtihon.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import uz.najottalim.imtihon.Entitys.Lavozim;
 import uz.najottalim.imtihon.Entitys.Malumot;
 
+import java.util.Optional;
 
-public interface MalumotRypository extends JpaRepository<Long, Malumot> {
+
+public interface MalumotRypository extends JpaRepository<Malumot, Long> {
 }
